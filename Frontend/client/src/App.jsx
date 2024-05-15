e1c015a7c5e3cdab0d457ed96f5b75e96096bbef
gunapriya07 <147796209+gunapriya07@users.noreply.github.com>
2024-05-15 11:04:35 +0530

    created reactapp

---
+++ Frontend/client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Hello Chittoor!</h1>
+      <p>Welcome to Chittoor Trails - your ultimate guide to exploring the scenic trails, historical routes, and cultural landmarks of the enchanting Chittoor district.</p>
+    </>
+  )
+}
+
+export default App
